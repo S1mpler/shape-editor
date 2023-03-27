4d239b494797fa9d94b4992738c7a84890a7e9e3
S1mpler <stas.dior@gmail.com>
2023-03-27 09:48:34 +0200

    Add github repo link to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,8 +12,7 @@ function NotFoundPage(): ReactElement {
             <a href="/" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Go back home
             </a>
-            {/* TODO: Add github link */}
-            <a href="/" className="text-sm font-semibold text-gray-900">
+            <a href="https://github.com/S1mpler/shape-editor" className="text-sm font-semibold text-gray-900">
               Read documentation <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
