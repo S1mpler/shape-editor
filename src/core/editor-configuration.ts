a6bc72c62a343400054e2503cc4116607f81cb7e
S1mpler <stas.dior@gmail.com>
2023-03-27 07:49:26 +0200

    Bring basic editor configuration into one place

---
+++ src/core/editor-configuration.ts
@@ -0,0 +1,11 @@
+import IPoint from './models/point.interface';
+
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = CANVAS_WIDTH * (window.innerHeight / window.innerWidth);
+
+const SPAWN_POINT: IPoint = {
+  x: CANVAS_WIDTH / 2,
+  y: CANVAS_HEIGHT / 2
+};
+
+export { CANVAS_WIDTH, CANVAS_HEIGHT, SPAWN_POINT };
